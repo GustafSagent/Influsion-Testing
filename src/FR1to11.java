33dd5d956bf5c5dc46158fbf05f3af4dfa76f79b
Gustaf Sagent <gws.gustaf@gmail.com>
2018-11-13 11:07:51 +0100

    FR2 ready. Added helpFunctions

--- src/FR1to11.java
+++ src/FR1to11.java
@@ -1,11 +1,14 @@
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,7 +23,6 @@ public class FR1to11{
 		// Change webdriver filepath to your own manually (Easy solution now in the start up phase)
 		// /Users/Gustaf/Desktop/SeleniumDrivers/chromedriver
 		System.setProperty("webdriver.chrome.driver", "/Users/Gustaf/Desktop/SeleniumDrivers/chromedriver");
-
 		browser= new ChromeDriver();
 		browser.get("http://localhost:8080/");   
 	}
@@ -32,85 +34,80 @@ public class FR1to11{
 	
 
 	
-//	@Test
-//	public void TestingTitle() {
-//
-//	String name = browser.getTitle();   
-//	assertEquals("Pumba",name);   
-//	
-//
-//	}
-//	
-//	
-//	@Test
-//	public void FR1() {
-//		try {
-//			Thread.sleep(200);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	List<WebElement> PopularFeed = browser.findElements(By.cssSelector(".popular-feed-content"));
-//	List<WebElement> PopularComponent = browser.findElements(By.className("popular-component-wrapper"));
-////	for (int i=0; i<PopularComponent.size();i++) {
-////		System.out.println(PopularComponent.get(i).getText());
-////		System.out.println("COUNT: " + i);	
-////	}
-////	System.out.println(PopularComponent.size());
-//	assertEquals(PopularComponent.size(),100);  
-//	assertEquals(PopularFeed.size(), 1);
-//	}
-//	
-
+	
+	
 	@Test
-	public void FR2() {
-		try {
+	public void FR1() throws InterruptedException {
+		
+		Thread.sleep(200);
+		
+		List<WebElement> PopularFeed = browser.findElements(By.cssSelector(".popular-feed-content"));
+		List<WebElement> PopularComponent = browser.findElements(By.className("popular-component-wrapper"));
+		assertEquals(PopularComponent.size(),100);  
+		assertEquals(PopularFeed.size(), 1);
+	}
+	
+
+	
+//Test Case 2 dived up into 3 test cases to get a better view. At the moment we do only check the first post and see if it works. 
+	@Test
+	public void FR2_1() throws InterruptedException {
+	
 			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		List<WebElement> YoutubeContent =browser.findElements(By.cssSelector("a[href*=youtube]"));
+		WebElement YoutubePost = YoutubeContent.get(0);
 		
-		
-		System.out.println(YoutubeContent);
-		for (int i=0; i<YoutubeContent.size();i++) {
-			
-			if (YoutubeContent.get(i).isDisplayed()) {
-				System.out.println("isDisplayed" + "  Before");
-			}
-			
-			
-			Actions actions = new Actions(browser);
-			actions.moveToElement(YoutubeContent.get(i));
-			
-			
-			
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			if (YoutubeContent.get(i).isDisplayed()) {
-				System.out.println("isDisplayed");
-			}
-			
-		//	YoutubeContent.get(i).click();
-			
-			
-		
-		
-		System.out.println(YoutubeContent.get(i));
-		
+			String videolink = YoutubePost.getAttribute("href");
+			((JavascriptExecutor) browser).executeScript("arguments[0].scrollIntoView(true);", YoutubePost);
+			((JavascriptExecutor) browser).executeScript("window.scrollBy(0,-50)","");
+			Thread.sleep(200);
+			YoutubePost.click();
+			String url = browser.getCurrentUrl();
+			System.out.println(url);
+			assertEquals(url,videolink);
 		}
 		
-		System.out.println(YoutubeContent.size());
-		assertEquals(1, 1);
 	
-		}
+	@Test
+	public void FR2_2() throws InterruptedException {
+			
+		Thread.sleep(1000);
+		
+		
+		List<WebElement> TwitterContent =browser.findElements(By.cssSelector("a[href*=twitter]"));	
+		WebElement TwitterPost = TwitterContent.get(0);
+
+		String postlink = TwitterPost.getAttribute("href");
+		((JavascriptExecutor) browser).executeScript("arguments[0].scrollIntoView(true);", TwitterPost);
+		((JavascriptExecutor) browser).executeScript("window.scrollBy(0,-50)","");
+		Thread.sleep(200);
+		TwitterPost.click();
+		String url = browser.getCurrentUrl();
+		System.out.println(url);
+		assertEquals(url,postlink);
+	}
+		
+		
+	@Test
+	public void FR2_3() throws InterruptedException {
+		
+		Thread.sleep(1000);
+			
+		List<WebElement> InstaContent =browser.findElements(By.cssSelector("a[href*=instagram]"));
+		WebElement InstaPost = InstaContent.get(0);
+		String Instalink = InstaPost.getAttribute("href");
+		((JavascriptExecutor) browser).executeScript("arguments[0].scrollIntoView(true);", InstaPost);
+		((JavascriptExecutor) browser).executeScript("window.scrollBy(0,-50)","");
+		Thread.sleep(200);
+		InstaPost.click();
+		String url = browser.getCurrentUrl();
+		System.out.println(url);
+		assertEquals(url,Instalink);
+			}
+		
+		
+
 
 
 }
