3135670b14dd3f05a4f22cbe3bee7c4d31469e92
Gustaf Sagent <gws.gustaf@gmail.com>
2018-11-20 08:46:28 +0100

    Added login function

--- src/helpFunctions.java
+++ src/helpFunctions.java
@@ -1,5 +1,8 @@
 import java.util.Random;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class helpFunctions {
 
 	
@@ -7,6 +10,15 @@ public class helpFunctions {
 	    Random rand = new Random();
 	    int randomNum = rand.nextInt((max - min) + 1) + min;
 	    return randomNum;
+	}
+	
+	public static void login(String username, String password, WebDriver browser) throws InterruptedException {
+		
+		
+		browser.findElement(By.cssSelector("[placeholder='Username']")).sendKeys(username);
+		browser.findElement(By.cssSelector("[placeholder='Password']")).sendKeys(password);
+		browser.findElement(By.xpath("//button[contains(text(),'Lets go into the wilderness!')]"));
+		Thread.sleep(1000);
 	}
 	
 }
