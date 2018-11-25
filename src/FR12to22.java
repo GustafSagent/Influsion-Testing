389b0080b47a1a01957be3be7f7e53cf7ab42281
Herman Eklund <heklund8@gmail.com>
2018-11-25 18:27:53 +0100

    sprint 3 testcases as ahas been implemented

--- src/FR12to22.java
+++ src/FR12to22.java
@@ -4,7 +4,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.junit.After;
@@ -14,6 +16,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class FR12to22{
@@ -23,8 +26,16 @@ public class FR12to22{
 	@Before
 	public void setUpTestEnviornment() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Herma/Desktop/SeleniumDriver/chromedriver.exe");
-		browser= new ChromeDriver();
-		browser.get("http://localhost:8080/");	
+		
+		Map<String, String> mobileEmulation = new HashMap<>();
+		mobileEmulation.put("deviceName", "Nexus 5");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+		browser = new ChromeDriver(chromeOptions);
+		
+		browser.get("http://13.93.37.179/");
+		login("test1","test1", browser);
+
 		Thread.sleep(500);		
 	}
 	
@@ -42,11 +53,7 @@ public class FR12to22{
 	
 	//FR13 following functionality
 	@Test
-	public void FR13() throws InterruptedException {
-		
-		helpFunctions.login("test1","test1", browser);
-		browser.get("http://localhost:8080/");	
-		Thread.sleep(500);		
+	public void FR13() throws InterruptedException {		
 		
 		List<WebElement> PopularComponent = browser.findElements(By.className("popular-component-wrapper"));
 		
@@ -64,7 +71,8 @@ public class FR12to22{
 		String nameKey = content[0];
 		
 		//go to follow page'
-		browser.get("http://localhost:8080/feed");
+		browser.findElement(By.className("blur-overlay")).click();
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-heart")).click();
 		Thread.sleep(500);
 
 		//see if there are any posts from this influencer
@@ -85,13 +93,9 @@ public class FR12to22{
 	//FR14 check order of posts
 	@Test
 	public void FR14() throws InterruptedException, ParseException, NoSuchElementException {
-	
-		helpFunctions.login("test1","test1", browser);
-		browser.get("http://localhost:8080/");
-		Thread.sleep(500);
 		
 		//go to follow page'
-		browser.get("http://localhost:8080/feed");
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-heart")).click();
 		Thread.sleep(500);
 		
 		List<WebElement> FeedComponent = browser.findElements(By.className("feed-component-wrapper"));
@@ -108,25 +112,20 @@ public class FR12to22{
 		
 		for (Date date : timestamps) {
 			int indx = timestamps.indexOf(date);
-			if (indx < timestamps.size()-1) {
-				if (!date.after(timestamps.get(indx+1))) {
+			if (indx < timestamps.size()-1) {	
+				if (date.compareTo(timestamps.get(indx+1)) < 0) {
 					correctOrder = false;
 				}	
 			}
 		}	
 		
-		assertEquals(correctOrder, true);
-
+		assertEquals(true, correctOrder);
 	}
 	
 	
 	//FR15 unfollow functionality
 	@Test
 	public void FR15() throws InterruptedException {
-		
-		helpFunctions.login("test1","test1", browser);
-		browser.get("http://localhost:8080/");
-		Thread.sleep(500);
 		
 		List<WebElement> PopularComponent = browser.findElements(By.className("popular-component-wrapper"));
 		
@@ -144,7 +143,8 @@ public class FR12to22{
 		String nameKey = content[0];
 		
 		//go to follow page'
-		browser.get("http://localhost:8080/feed");
+		browser.findElement(By.className("blur-overlay")).click();
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-heart")).click();
 		Thread.sleep(500);
 
 		//see if there are any posts from this influencer
@@ -164,21 +164,79 @@ public class FR12to22{
 	
 	
 	//FR16 settings page and functionality
+	@Test
+	public void FR16() throws InterruptedException {		
+				
+		//go to settings page'
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-cogs")).click();
+		Thread.sleep(500);
+		
+		WebElement settings = browser.findElement(By.tagName("a"));
+		String settingsPage = settings.getText();
+		
+		boolean exists = false;
+		
+		if (settingsPage.equals("Edit information")) {
+			settings.click();
+			exists = true;
+		}
+		
+		assertTrue(exists);
+	}	
 	
-	
-	//FR17 TV operator settings page and functionality
-	
-	
+
 	//FR18 Logout
+	@Test
+	public void FR18() throws InterruptedException {		
+				
+		//go to settings page'
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-cogs")).click();
+		browser.findElement(By.tagName("button")).click();
+		assertTrue(browser.findElements(By.className("login")).size() > 0);
+
+	}
 	
 	
 	//FR19 Change personal information in settings menu
-	
+	@Test
+	public void FR19() throws InterruptedException {		
+		
+		//go to settings page'
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-cogs")).click();
+
+		// not done because not implemented
+		
+	}
+
 	
 	//20-21 Delete account functionality
-	
+	@Test
+	public void FR20_21() throws InterruptedException {		
+		
+		//go to settings page'
+		browser.findElement(By.className("subFooter")).findElement(By.className("fa-cogs")).click();
+		List<WebElement> buttons = browser.findElements(By.tagName("a"));
+		ArrayList<String> buttonText = new ArrayList<String>();
+		for (WebElement button : buttons) {
+			if (button.getText().equals("Delete account")) {
+				button.click();
+				Thread.sleep(1000);		
+			}
+		}
+		
+		//not done because not implemented
+	}
+
 	
 	//FR22 Promote Specific Influencer
+	
+	public static void login(String username, String password, WebDriver browser) throws InterruptedException {
+		
+		browser.findElement(By.cssSelector("[placeholder='Username']")).sendKeys(username);
+		browser.findElement(By.cssSelector("[placeholder='Password']")).sendKeys(password);
+		browser.findElement(By.xpath("//button[contains(text(),'Lets go into the wilderness!')]")).click();;
+		Thread.sleep(1000);
+	}
 	
 }
 
